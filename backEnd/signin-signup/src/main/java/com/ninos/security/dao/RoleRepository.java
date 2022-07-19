fe2c0b785fdcfc4adc4a302ec0313b4f02f3de1d
NinoosMoshi <nalkaton@gmail.com>
2022-07-19 09:22:54 -0400

    Spring - config and repository Closes #2

---
+++ backEnd/signin-signup/src/main/java/com/ninos/security/dao/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.ninos.security.dao;
+
+import com.ninos.security.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role,Long> {
+}
