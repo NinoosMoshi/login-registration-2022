eb36cfadcb24c98f71c36680cb5b8ce00bf279ab
NinoosMoshi <nalkaton@gmail.com>
2022-07-19 08:46:01 -0400

    Spring - user and role classes #1

---
+++ backEnd/signin-signup/src/main/java/com/ninos/security/model/Role.java
@@ -0,0 +1,26 @@
+package com.ninos.security.model;
+
+import lombok.*;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "role_name")
+    private String roleName;
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+}
